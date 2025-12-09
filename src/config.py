2cbcd0fc291470360783aff473c35caf017496b7
Ingrid0707 <ferreiradecarvalhoingridtaina@gmail.com>
2025-12-09 01:49:05 -0300

    Primeiro commit do projeto Flask TarefasApp

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = "minha_chave_secreta"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///tasks.db"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "minha_chave_secreta_local")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///tasks.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False

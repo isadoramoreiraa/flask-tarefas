0611ed0ea2096af6b3d54f09bcadcefdc15bec77
Ingrid0707 <ferreiradecarvalhoingridtaina@gmail.com>
2025-12-09 02:48:51 -0300

    primeiro

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "minha_chave_secreta"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "minha_chave_secreta")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data", "tasks.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False

3b5d76f27f2101134d9ac75c382430deac233af3
Ingrid0707 <ferreiradecarvalhoingridtaina@gmail.com>
2025-12-09 02:22:43 -0300

    perdida

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from flask import Flask
-from src.extensions import db
-from src.config import Config
-
-from src.routes.auth_routes import auth_routes
-from src.routes.task_routes import task_routes
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -16,5 +9,9 @@ def create_app():
 
     app.register_blueprint(auth_routes, url_prefix="/auth")
     app.register_blueprint(task_routes, url_prefix="/tasks")
+
+    @app.route("/")
+    def home():
+        return "Servidor rodando! SQLite conectado."
 
     return app
--- src/config.py
+++ src/config.py
@@ -1,12 +1,8 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "minha_chave_secreta")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "sqlite:///" + os.path.join(BASE_DIR, "tasks.db")
-    )
-
+    SECRET_KEY = "minha_chave_secreta"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data", "tasks.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
